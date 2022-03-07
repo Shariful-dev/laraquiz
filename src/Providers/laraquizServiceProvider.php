da4600523600b12fb5f29fb55af9bf6b8d0fb7af
MD Shariful islam <sharifulmd751@gmail.com>
2022-03-07 23:47:58 +0600

    first commit

---
+++ src/Providers/laraquizServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace shariful\laraquiz\Providers;
+
+use illuminate\support\ServiceProvider;
+
+class laraquizServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+    public function register()
+    {
+        
+    }
+}
